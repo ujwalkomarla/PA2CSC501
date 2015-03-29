cdd3992b8c3a6a4e9ae9344e244e400ae835cab5
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2015-03-29 10:37:10 -0400

    XMUNMAP

--- paging/xm.c
+++ paging/xm.c
@@ -34,7 +34,7 @@ SYSCALL xmmap(int virtpage, bsd_t source, int npages)
 #ifdef DEBUGuser
 ProcBSlist *t = proctab[currpid].pBSlist;
 while(t!=NULL){
-kprintf("%x  Store :%d   VHPNO: %d NPAGES: %d->",t,t->store,t->vhpno,t->vhpnpages);
+kprintf("%x  Store :%d   VHPNO: %d NPAGES: %d\n",t,t->store,t->vhpno,t->vhpnpages);
 t = t->next;
 }
 kprintf("\n");
@@ -63,20 +63,60 @@ SYSCALL xmunmap(int virtpage )
   /* sanity check ! */
   if ( (virtpage < 4096) ){ 
  	restore(ps);
-	kprintf("xmummap call error: virtpage (%d) invalid! \n", virtpage);
+	kprintf("xmunmap call error: virtpage (%d) invalid! \n", virtpage);
 	return SYSERR;
   }
-	if(proctab[currpid].vhpno<virtpage && (proctab[currpid].vhpno+proctab[currpid].vhpnpages) > virtpage){//Release heap
+	if(proctab[currpid].vhpno<=virtpage && (proctab[currpid].vhpno+proctab[currpid].vhpnpages) > virtpage){//Release heap
 		bs_id = proctab[currpid].store;
 		vhpno = proctab[currpid].vhpno;
 		tpg = proctab[currpid].vhpnpages;
+		{//REPEATED in ELSE
+			used_frm_list **t_frm;
+			t_frm = &usedhead;
+			while(*t_frm !=NULL){
+				if(frm_tab[(*t_frm)->frameno].fr_pid  = currpid){
+					if(vhpno<=frm_tab[(*t_frm)->frameno].fr_vpno && vhpno+vhpnpages>frm_tab[(*t_frm)->frameno].fr_vpno){
+
+						free_frm((*t_frm)->frameno);
+						free_frm_list* t1_frm = *t_frm ;
+						*t_frm  = (*t_frm )->next;
+						t1_frm->next = freehead;
+						freehead = t1_frm;
+						continue;
+					}
+				}
+				t_frm = &(*t_frm)->next;
+			}	
+				bsm_tab[bs_id].bs_vpno[currpid] = -1;
+				proctab[currpid].vhpno = -1;//OR t->vhpno = -1
+		}
 	}else{//File
 		ProcBSlist *t = proctab[currpid].pBSlist;
 		while(t != NULL){
-			if(t->vhpno<virtpage && (t->vhpno + t->vhpnpages)>virtpage){
+			if(t->vhpno<=virtpage && (t->vhpno + t->vhpnpages)>virtpage){
 				bs_id = t->store;
 				vhpno  = t->vhpno;
 				tpg = t->vhpnpages;
+				{//REPEATED in IF
+					used_frm_list **t_frm;
+					t_frm = &usedhead;
+					while(*t_frm !=NULL){
+						if(frm_tab[(*t_frm)->frameno].fr_pid  = currpid){
+							if(vhpno<=frm_tab[(*t_frm)->frameno].fr_vpno && vhpno+vhpnpages>frm_tab[(*t_frm)->frameno].fr_vpno){
+
+								free_frm((*t_frm)->frameno);
+								free_frm_list* t1_frm = *t_frm ;
+								*t_frm  = (*t_frm )->next;
+								t1_frm->next = freehead;
+								freehead = t1_frm;
+								continue;
+							}
+						}
+						t_frm = &(*t_frm)->next;
+					}	
+						bsm_tab[bs_id].bs_vpno[currpid] = -1;
+						t->vhpno = -1;
+				}
 				break;
 			}
 			t = t->next;
@@ -84,12 +124,12 @@ SYSCALL xmunmap(int virtpage )
 
 		if(t == NULL){restore(ps); return SYSERR;}
 	}
-	for(i=0;i<NFRAMES;i++){
+	/*for(i=0;i<NFRAMES;i++){
 		if(frm_tab[i].fr_pid==currpid){
 			if(vhpno<virtpage&&vhpno+tpg>virtpage){
-free_frm(i);
-}
+			free_frm(i);
+			}
 		}
-	}
-	//remove entry from page table(use free_frm)
+	}*/
+
 }
